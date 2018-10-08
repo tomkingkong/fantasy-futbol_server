6f24072f266b25f9888730bf57a19885f1cf351b
sojurner <pykim0591@gmail.com>
2018-10-08 15:47:13 -0600

    Nightmare scrape

--- soccorscraper.js
+++ soccorscraper.js
@@ -1,31 +1,26 @@
-const Nightmare = require('nightmare')
+const Nightmare = require('nightmare');
 const nightmare = Nightmare({
-	show: true
-})
-const fs = require('file-system')
-
+  show: true
+});
+const fs = require('file-system');
+const $ = require('jQuery');
 
 nightmare
-	.goto('https://en.wikipedia.org/wiki/2018_FIFA_World_Cup_squads')
-	.wait(500)
-	.evaluate(() => {
-		const datas = [...document.querySelectorAll('h3')]
+  .goto('https://en.wikipedia.org/wiki/2018_FIFA_World_Cup_squads')
+  .wait(500)
+  .evaluate(() => {
+    const datas = $('.mw-parser-output')
+      .find('h3')
+      .find('.mw-headline')
+      .text();
 
-		const results = datas.map(data => {
-			let countryName = data.querySelector('.mw-body-content')
-			// let price = data.querySelector('.currency-value').innerText
-			return {
-				countryName
-			}
-		})
-		return results
-	})
-
-	.end()
-	.then(result => {
-		fs.writeFile('./countryNames.js', JSON.stringify(result), function (err) {})
-		console.log('worked')
-	})
-	.catch(error => {
-		console.error('Search failed:', error)
-	})
+    return datas;
+  })
+  .end()
+  .then(result => {
+    fs.writeFile('./countryNames.js', JSON.stringify(result), function(err) {});
+    console.log(result);
+  })
+  .catch(error => {
+    console.error('Search failed:', error);
+  });
