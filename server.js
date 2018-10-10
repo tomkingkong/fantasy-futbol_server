d64848d140620d8cbd5aec88ee3eea9a51308f99
sojurner <pykim0591@gmail.com>
2018-10-10 07:27:32 -0600

    List of desired keys

--- server.js
+++ server.js
@@ -42,14 +42,52 @@ app.get('/', (request, response) => {
     newList.push(newPlayers);
   }
 
-  //   for (let key in keys) {
-  //     newPlayers[keys[key]] =
-  //       participatingPlayers[player][
-  //         Object.keys(participatingPlayers[player])[key]
-  //       ];
-  //   }
-  //   newList.push(newPlayers);
-  // }
+  let actualKeys = [
+    'Name',
+    'Age',
+    'Photo',
+    'Nationality',
+    'Club',
+    'Overall',
+    'Potential',
+    'Value',
+    'Wage',
+    'Acceleration',
+    'Aggression',
+    'Agility',
+    'Balance',
+    'Ball_control',
+    'Composure',
+    'Crossing',
+    'Curve',
+    'Dribbling',
+    'Finishing',
+    'Free_kick_accuracy',
+    'GK_diving',
+    'GK_handling',
+    'GK_kicking',
+    'GK_positioning',
+    'GK_reflexes',
+    'Heading_accuracy',
+    'Interceptions',
+    'Jumping',
+    'Long_passing',
+    'Long_shots',
+    'Marking',
+    'Penalties',
+    'Positioning',
+    'Reactions',
+    'Short_passing',
+    'Shot_power',
+    'Sliding_tackle',
+    'Sprint_speed',
+    'Stamina',
+    'Standing_tackle',
+    'Strength',
+    'Vision',
+    'Volleys',
+    'Preferred_Positions'
+  ];
 
   fs.writeFileSync('./data/goalies.json', JSON.stringify(goalies, null, 4));
 
