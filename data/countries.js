b3ab585840b9abbddd840d3999ca4b275171029d
sojurner <pykim0591@gmail.com>
2018-10-10 07:29:18 -0600

    Add flag and flag link to countries obj

--- data/countries.js
+++ data/countries.js
@@ -1,130 +1,162 @@
 module.exports = [
   {
     name: 'Argentina',
-    group: 'D'
+    group: 'D',
+    flag: 'https://cdn.sofifa.org/flags/52.png'
   },
   {
     name: 'Australia',
-    group: 'C'
+    group: 'C',
+    flag: 'https://cdn.sofifa.org/flags/195.png'
   },
   {
     name: 'Belgium',
-    group: 'G'
+    group: 'G',
+    flag: 'https://cdn.sofifa.org/flags/7.png'
   },
   {
     name: 'Brazil',
-    group: 'E'
+    group: 'E',
+    flag: 'https://cdn.sofifa.org/flags/54.png'
   },
   {
     name: 'Colombia',
-    group: 'H'
+    group: 'H',
+    flag: 'https://cdn.sofifa.org/flags/56.png'
   },
   {
     name: 'Costa Rica',
-    group: 'E'
+    group: 'E',
+    flag: 'https://cdn.sofifa.org/flags/72.png'
   },
   {
     name: 'Croatia',
-    group: 'D'
+    group: 'D',
+    flag: 'https://cdn.sofifa.org/flags/10.png'
   },
   {
     name: 'Denmark',
-    group: 'C'
+    group: 'C',
+    flag: 'https://cdn.sofifa.org/flags/13.png'
   },
   {
     name: 'Egypt',
-    group: 'C'
+    group: 'C',
+    flag: 'https://cdn.sofifa.org/flags/111.png'
   },
   {
     name: 'England',
-    group: 'G'
+    group: 'G',
+    flag: 'https://cdn.sofifa.org/flags/14.png'
   },
   {
     name: 'France',
-    group: 'C'
+    group: 'C',
+    flag: 'https://cdn.sofifa.org/flags/18.png'
   },
   {
     name: 'Germany',
-    group: 'F'
+    group: 'F',
+    flag: 'https://cdn.sofifa.org/flags/21.png'
   },
   {
     name: 'Iceland',
-    group: 'D'
+    group: 'D',
+    flag: 'https://cdn.sofifa.org/flags/24.png'
   },
   {
     name: 'Iran',
-    group: 'B'
+    group: 'B',
+    flag: 'https://cdn.sofifa.org/flags/161.png'
   },
   {
     name: 'Japan',
-    group: 'H'
+    group: 'H',
+    flag: 'https://cdn.sofifa.org/flags/163.png'
   },
   {
     name: 'Korea Republic',
-    group: 'F'
+    group: 'F',
+    flag: 'https://cdn.sofifa.org/flags/167.png'
   },
   {
     name: 'Mexico',
-    group: 'F'
+    group: 'F',
+    flag: 'https://cdn.sofifa.org/flags/83.png'
   },
   {
     name: 'Morocco',
-    group: 'B'
+    group: 'B',
+    flag: 'https://cdn.sofifa.org/flags/129.png'
   },
   {
     name: 'Nigeria',
-    group: 'D'
+    group: 'D',
+    flag: 'https://cdn.sofifa.org/flags/133.png'
   },
   {
     name: 'Panama',
-    group: 'G'
+    group: 'G',
+    flag: 'https://cdn.sofifa.org/flags/87.png'
   },
   {
     name: 'Peru',
-    group: 'C'
+    group: 'C',
+    flag: 'https://cdn.sofifa.org/flags/59.png'
   },
   {
     name: 'Poland',
-    group: 'H'
+    group: 'H',
+    flag: 'https://cdn.sofifa.org/flags/37.png'
   },
   {
     name: 'Portugal',
-    group: 'B'
+    group: 'B',
+    flag: 'https://cdn.sofifa.org/flags/38.png'
   },
   {
     name: 'Russia',
-    group: 'C'
+    group: 'C',
+    flag: 'https://cdn.sofifa.org/flags/40.png'
   },
   {
     name: 'Saudi Arabia',
-    group: 'C'
+    group: 'C',
+    flag: 'https://cdn.sofifa.org/flags/183.png'
   },
   {
     name: 'Senegal',
-    group: 'H'
+    group: 'H',
+    flag: 'https://cdn.sofifa.org/flags/136.png'
   },
   {
     name: 'Serbia',
-    group: 'E'
+    group: 'E',
+    flag: 'https://cdn.sofifa.org/flags/51.png'
   },
   {
     name: 'Spain',
-    group: 'B'
+    group: 'B',
+    flag: 'https://cdn.sofifa.org/flags/45.png'
   },
   {
     name: 'Sweden',
-    group: 'F'
+    group: 'F',
+    flag: 'https://cdn.sofifa.org/flags/46.png'
   },
   {
     name: 'Switzerland',
-    group: 'E'
+    group: 'E',
+    flag: 'https://cdn.sofifa.org/flags/47.png'
   },
   {
     name: 'Tunisia',
-    group: 'G'
+    group: 'G',
+    flag: 'https://cdn.sofifa.org/flags/145.png'
   },
   {
     name: 'Uruguay',
-    group: 'C'
+    group: 'C',
+    flag: 'https://cdn.sofifa.org/flags/60.png'
   }
 ];
