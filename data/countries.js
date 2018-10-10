5b3f77c28eda093782d5268b4de8e4be20127614
sojurner <pykim0591@gmail.com>
2018-10-10 05:42:58 -0600

    Create corresponding countries.js for json file

---
+++ data/countries.js
@@ -0,0 +1,130 @@
+module.exports = [
+  {
+    name: 'Argentina',
+    group: 'D'
+  },
+  {
+    name: 'Australia',
+    group: 'C'
+  },
+  {
+    name: 'Belgium',
+    group: 'G'
+  },
+  {
+    name: 'Brazil',
+    group: 'E'
+  },
+  {
+    name: 'Colombia',
+    group: 'H'
+  },
+  {
+    name: 'Costa Rica',
+    group: 'E'
+  },
+  {
+    name: 'Croatia',
+    group: 'D'
+  },
+  {
+    name: 'Denmark',
+    group: 'C'
+  },
+  {
+    name: 'Egypt',
+    group: 'C'
+  },
+  {
+    name: 'England',
+    group: 'G'
+  },
+  {
+    name: 'France',
+    group: 'C'
+  },
+  {
+    name: 'Germany',
+    group: 'F'
+  },
+  {
+    name: 'Iceland',
+    group: 'D'
+  },
+  {
+    name: 'Iran',
+    group: 'B'
+  },
+  {
+    name: 'Japan',
+    group: 'H'
+  },
+  {
+    name: 'Korea Republic',
+    group: 'F'
+  },
+  {
+    name: 'Mexico',
+    group: 'F'
+  },
+  {
+    name: 'Morocco',
+    group: 'B'
+  },
+  {
+    name: 'Nigeria',
+    group: 'D'
+  },
+  {
+    name: 'Panama',
+    group: 'G'
+  },
+  {
+    name: 'Peru',
+    group: 'C'
+  },
+  {
+    name: 'Poland',
+    group: 'H'
+  },
+  {
+    name: 'Portugal',
+    group: 'B'
+  },
+  {
+    name: 'Russia',
+    group: 'C'
+  },
+  {
+    name: 'Saudi Arabia',
+    group: 'C'
+  },
+  {
+    name: 'Senegal',
+    group: 'H'
+  },
+  {
+    name: 'Serbia',
+    group: 'E'
+  },
+  {
+    name: 'Spain',
+    group: 'B'
+  },
+  {
+    name: 'Sweden',
+    group: 'F'
+  },
+  {
+    name: 'Switzerland',
+    group: 'E'
+  },
+  {
+    name: 'Tunisia',
+    group: 'G'
+  },
+  {
+    name: 'Uruguay',
+    group: 'C'
+  }
+];
