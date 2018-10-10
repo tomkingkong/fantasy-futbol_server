35b7d3409d946b09b47310f718edf12392bdb83d
benjaminpaige <benjamin.paige@gmail.com>
2018-10-10 13:47:09 -0600

    Add user schema and seed data

--- server.js
+++ server.js
@@ -12,138 +12,164 @@ app.use(bodyparser.json());
 const port = process.env.PORT || 3000;
 
 app.get('/', (request, response) => {
-  // const playersList = JSON.parse(fs.readFileSync('./data/players.json'));
-  // const participatingPlayers = playersList.reduce((players, player) => {
-  //   countries.forEach(country => {
-  //     if (country.name === player.Nationality) {
-  //       players.push(player);
-  //     }
-  //   });
-  //   return players;
-  // }, []);
-  // const keys = Object.keys(participatingPlayers[0]).map(key =>
-  //   key.replace(/ /g, '_')
-  // );
-  // let newList = [];
-  // for (let player in participatingPlayers) {
-  //   var newPlayers = {};
-  //   for (let key in keys) {
-  //     newPlayers[keys[key]] =
-  //       participatingPlayers[player][
-  //         Object.keys(participatingPlayers[player])[key]
-  //       ];
-  //   }
-  //   newList.push(newPlayers);
-  // }
-  // let actualKeys = [
-  //   'Name',
-  //   'Age',
-  //   'Photo',
-  //   'Nationality',
-  //   'Club',
-  //   'Overall',
-  //   'Potential',
-  //   'Value',
-  //   'Wage',
-  //   'Acceleration',
-  //   'Aggression',
-  //   'Agility',
-  //   'Balance',
-  //   'Ball_control',
-  //   'Composure',
-  //   'Crossing',
-  //   'Curve',
-  //   'Dribbling',
-  //   'Finishing',
-  //   'Free_kick_accuracy',
-  //   'GK_diving',
-  //   'GK_handling',
-  //   'GK_kicking',
-  //   'GK_positioning',
-  //   'GK_reflexes',
-  //   'Heading_accuracy',
-  //   'Interceptions',
-  //   'Jumping',
-  //   'Long_passing',
-  //   'Long_shots',
-  //   'Marking',
-  //   'Penalties',
-  //   'Positioning',
-  //   'Reactions',
-  //   'Short_passing',
-  //   'Shot_power',
-  //   'Sliding_tackle',
-  //   'Sprint_speed',
-  //   'Stamina',
-  //   'Standing_tackle',
-  //   'Strength',
-  //   'Vision',
-  //   'Volleys',
-  //   'Preferred_Positions'
-  // ];
-  // const players = newList.map(res => {
-  //   let obj = {};
-  //   actualKeys.forEach(key => {
-  //     if (res[key]) {
-  //       obj[key] = res[key];
-  //     }
-  //   });
-  //   obj['Positions'] = obj.Preferred_Positions;
-  //   delete obj.Preferred_Positions;
-  //   return obj;
-  // });
-  // console.log(players);
-  // fs.writeFileSync(
-  //   './data/scrapedPlayers.json',
-  //   JSON.stringify(players, null, 4),
-  //   function(err) {}
-  // );
+	// const playersList = JSON.parse(fs.readFileSync('./data/players.json'));
+	// const participatingPlayers = playersList.reduce((players, player) => {
+	//   countries.forEach(country => {
+	//     if (country.name === player.Nationality) {
+	//       players.push(player);
+	//     }
+	//   });
+	//   return players;
+	// }, []);
+	// const keys = Object.keys(participatingPlayers[0]).map(key =>
+	//   key.replace(/ /g, '_')
+	// );
+	// let newList = [];
+	// for (let player in participatingPlayers) {
+	//   var newPlayers = {};
+	//   for (let key in keys) {
+	//     newPlayers[keys[key]] =
+	//       participatingPlayers[player][
+	//         Object.keys(participatingPlayers[player])[key]
+	//       ];
+	//   }
+	//   newList.push(newPlayers);
+	// }
+	// let actualKeys = [
+	//   'Name',
+	//   'Age',
+	//   'Photo',
+	//   'Nationality',
+	//   'Club',
+	//   'Overall',
+	//   'Potential',
+	//   'Value',
+	//   'Wage',
+	//   'Acceleration',
+	//   'Aggression',
+	//   'Agility',
+	//   'Balance',
+	//   'Ball_control',
+	//   'Composure',
+	//   'Crossing',
+	//   'Curve',
+	//   'Dribbling',
+	//   'Finishing',
+	//   'Free_kick_accuracy',
+	//   'GK_diving',
+	//   'GK_handling',
+	//   'GK_kicking',
+	//   'GK_positioning',
+	//   'GK_reflexes',
+	//   'Heading_accuracy',
+	//   'Interceptions',
+	//   'Jumping',
+	//   'Long_passing',
+	//   'Long_shots',
+	//   'Marking',
+	//   'Penalties',
+	//   'Positioning',
+	//   'Reactions',
+	//   'Short_passing',
+	//   'Shot_power',
+	//   'Sliding_tackle',
+	//   'Sprint_speed',
+	//   'Stamina',
+	//   'Standing_tackle',
+	//   'Strength',
+	//   'Vision',
+	//   'Volleys',
+	//   'Preferred_Positions'
+	// ];
+	// const players = newList.map(res => {
+	//   let obj = {};
+	//   actualKeys.forEach(key => {
+	//     if (res[key]) {
+	//       obj[key] = res[key];
+	//     }
+	//   });
+	//   obj['Positions'] = obj.Preferred_Positions;
+	//   delete obj.Preferred_Positions;
+	//   return obj;
+	// });
+	// console.log(players);
+	// fs.writeFileSync(
+	//   './data/scrapedPlayers.json',
+	//   JSON.stringify(players, null, 4),
+	//   function(err) {}
+	// );
 });
 
 app.get('/api/v1/countries', (request, response) => {
-  database('countries')
-    .then(countries => response.status(200).json(countries))
-    .catch(error => {
-      response.status(500).json({ error });
-    });
+	database('countries')
+		.then(countries => response.status(200).json(countries))
+		.catch(error => {
+			response.status(500).json({
+				error
+			});
+		});
 });
 
 app.get('/api/v1/players', (request, response) => {
-  database('players')
-    .then(players => response.status(200).json(players))
-    .catch(error => {
-      response.status(500).json({ error });
-    });
+	database('players')
+		.then(players => response.status(200).json(players))
+		.catch(error => {
+			response.status(500).json({
+				error
+			});
+		});
+});
+
+app.get('/api/v1/users', (request, response) => {
+	database('users')
+		.then(users => response.status(200).json(users))
+		.catch(error => {
+			response.status(500).json({
+				error
+			});
+		});
 });
 
 app.get('/api/v1/players/:id', (request, response) => {
-  const { id } = request.params;
-  database('players')
-    .where('id', id)
-    .then(player => {
-      player.length
-        ? response.status(200).json(player)
-        : reponse.status(404).send({ error: 'player does not exist' });
-    })
-    .catch(error => {
-      response.status(500).json({ error });
-    });
+	const {
+		id
+	} = request.params;
+	database('players')
+		.where('id', id)
+		.then(player => {
+			player.length ?
+				response.status(200).json(player) :
+				reponse.status(404).send({
+					error: 'player does not exist'
+				});
+		})
+		.catch(error => {
+			response.status(500).json({
+				error
+			});
+		});
 });
 
 app.get('/api/v1/country/:id/players', (request, response) => {
-  const { id } = request.params;
-  database('players')
-    .where('country_id', id)
-    .then(player => {
-      player.length
-        ? response.status(200).json(player)
-        : response.status(404).send({ error: 'team does not exist' });
-    })
-    .catch(error => {
-      response.status(500).json({ error });
-    });
+	const {
+		id
+	} = request.params;
+	database('players')
+		.where('country_id', id)
+		.then(player => {
+			player.length ?
+				response.status(200).json(player) :
+				response.status(404).send({
+					error: 'team does not exist'
+				});
+		})
+		.catch(error => {
+			response.status(500).json({
+				error
+			});
+		});
 });
 
 app.listen(port, () => {
-  console.log('server is listening on 3000');
+	console.log('server is listening on 3000');
 });
